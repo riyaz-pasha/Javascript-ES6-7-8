fcb1e43a493963e2e388654ed0483691f5deb2d9
riyazpasha <riyazpasha880@gmail.com>
2020-02-23 22:46:23 +0530

    [Riyaz] Cann't reassign const

---
+++ Const-Let.js
@@ -0,0 +1,14 @@
+//Let
+let age = 20;
+
+console.log("age", age);
+
+age += 1; //Can be reassigned
+console.log("new age", age);
+
+//Const
+const name = "Riyaz";
+
+name = "Hahaha.. I'm changing your name";
+
+console.log(name);
