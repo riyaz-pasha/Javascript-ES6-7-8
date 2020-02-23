85bfef68418572baaa859a259bb3dd8129231ed9
riyazpasha <riyazpasha880@gmail.com>
2020-02-23 23:47:12 +0530

    [Riyaz] object literals

---
+++ Object-literals.js
@@ -0,0 +1,8 @@
+const firstName = "Riyaz";
+const lastName = "Mohammed";
+
+const Person1 = { firstName: firstName, lastName: lastName }
+console.log("Per1", Person1);
+
+const Person = { firstName, lastName }
+console.log("Person", Person);
