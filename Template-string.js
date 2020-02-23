496cd6e70137ab03b443149db300f7db9c3d6cc2
riyazpasha <riyazpasha880@gmail.com>
2020-02-23 23:05:25 +0530

    [Riyaz] Template strings

---
+++ Template-string.js
@@ -0,0 +1,6 @@
+// Template string
+const greetOld = (name) => "Hello " + name + "!";
+console.log("greetOld", greetOld("Riyaz"));
+
+const greet = (name) => `Hello ${name}!`;
+console.log("greet", greet("Riyaz"));
