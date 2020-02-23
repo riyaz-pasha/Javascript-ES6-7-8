3f78471c90709fa61df8696250f34ae226d1264d
riyazpasha <riyazpasha880@gmail.com>
2020-02-23 22:55:18 +0530

    [Riyaz] Arrow Functions shorthand implementation

--- Arrow-functions.js
+++ Arrow-functions.js
@@ -9,3 +9,7 @@ console.log(addFn(2, 3));
 const addArrowFn = (num1, num2) => { return num1 + num2; }
 
 console.log(addArrowFn(2, 3));
+
+const addArrowFnShortHand = (num1, num2) => num1 + num2;
+
+console.log("addArrowFnShortHand ", addArrowFnShortHand(2, 3));
