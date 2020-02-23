1ad8bb9f7af3e86151ebe855b8763af60f0fbe37
riyazpasha <riyazpasha880@gmail.com>
2020-02-23 23:00:33 +0530

    [Riyaz] Arrow Functions inline

--- Arrow-functions.js
+++ Arrow-functions.js
@@ -13,3 +13,11 @@ console.log(addArrowFn(2, 3));
 const addArrowFnShortHand = (num1, num2) => num1 + num2;
 
 console.log("addArrowFnShortHand ", addArrowFnShortHand(2, 3));
+
+//Inline functions
+
+console.log("Inline function", function (num1, num2) {
+    return num1 + num2
+}(3, 4))
+
+console.log("Inline Arrow function", ((num1, num2) => { return num1 + num2 })(1, 2))
