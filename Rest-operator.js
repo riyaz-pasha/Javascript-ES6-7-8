59ca7ff53e0836b3d9c71d2ab7e7f16183eb4d91
riyazpasha <riyazpasha880@gmail.com>
2020-02-23 23:28:26 +0530

    [Riyaz] rest operator

---
+++ Rest-operator.js
@@ -0,0 +1,11 @@
+const restEx = (...all) => console.log("all", all);
+
+restEx(1, 2, 3);
+restEx(1, 2, 3, 4, 5);
+
+const restEx2 = (first, ...exceptFirst) => console.log("exceptFirst", exceptFirst);
+
+restEx2();
+restEx2(1);
+restEx2(1, 2, 3);
+restEx2(1, 2, 3, 4, 5);
