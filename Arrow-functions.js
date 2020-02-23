80c3a523407ffa72276b8b024db12a51d7e0ec4f
riyazpasha <riyazpasha880@gmail.com>
2020-02-23 22:52:48 +0530

    [Riyaz] Arrow Functions implementation

---
+++ Arrow-functions.js
@@ -0,0 +1,11 @@
+//Arrow Functions
+
+function addFn(num1, num2) {
+    return (num1 + num2);
+}
+
+console.log(addFn(2, 3));
+
+const addArrowFn = (num1, num2) => { return num1 + num2; }
+
+console.log(addArrowFn(2, 3));
